e527e63ce8d8171651b4f051466eeaca8b9f867a
DeKingRey <tigermiguel456@gmail.com>
2026-02-12 11:26:32 +1300

    Database

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,6 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # Home Page
+
+    con = sqlite3.connect("recipes.db")
+    cursor = con.cursor()
+
+    cursor.execute("SELECT sql FROM sqlite_master WHERE type='table'")
+
+    for row in cursor.fetchall():
+        print(row[0])
+
     return "Hello again!"
 
 
